f785096d90ae184e33bdd079995f5ed3c3d8f50e
Serhat Ömer Rençber <35541081+KanKa65@users.noreply.github.com>
2020-06-26 02:01:02 +0200

    Update follow_fav_retweet.py

--- follow_fav_retweet.py
+++ follow_fav_retweet.py
@@ -4,7 +4,7 @@ from bots.configtweepy import set_api
 import json
 import time
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
+log = logging.getLogger()
 
 class Listener(tweepy.StreamListener):
     def __init__(self, api):
@@ -17,7 +17,7 @@ class Listener(tweepy.StreamListener):
         return True
 
     def on_status(self, tweet):
-        logger.info(f"Processing tweet id {tweet.id}")
+        log.info(f"Processing tweet id {tweet.id}")
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
             # If this tweet is  reply and it's mine, skip tweet
@@ -25,41 +25,41 @@ class Listener(tweepy.StreamListener):
         if not tweet.favorited:
             # If it is not Liked, Like it.
             try:
-                logger.info(f"Add favorite {tweet.id}")
+                log.info(f"Add favorite {tweet.id}")
 
                 tweet.favorite()
                 time.sleep(1 * 30)
 
             except Exception as e:
-                logger.error("Error on favorite", exc_info=True)
+                log.error("Error on favorite", exc_info=True)
         if not tweet.retweeted:
             # If it is not retweeted. Retweet it.
             try:
-                logger.info(f"Retweeting  {tweet.id}")
+                log.info(f"Retweeting  {tweet.id}")
 
                 tweet.retweet()
                 time.sleep(1 * 30)
 
             except Exception as e:
-                logger.error("Error on retweet", exc_info=True)
+                log.error("Error on retweet", exc_info=True)
 
         if not tweet.user.following:
             try:
-                logger.info(f"Following  tweet user {tweet.user}")
+                log.info(f"Following  tweet user {tweet.user}")
 
                 tweet.user.follow()
                 time.sleep(1 * 30)
 
             except Exception as e:
-                logger.error("Error on following", exc_info=True)
+                log.error("Error on following", exc_info=True)
     def on_error(self, status):
-        logger.error(status)
+        log.error(status)
 
 def main(keywords):
     api = set_api()
     listener = Listener(api)
-    stream = tweepy.Stream(api.auth, listener)
-    stream.filter(track=keywords, languages=["tr"])
+    streaming = tweepy.Stream(api.auth, listener)
+    streaming.filter(track=keywords, languages=["tr"])
 
 if __name__ == "__main__":
     main(["FatihTerzioglu AcilTahliye"])
